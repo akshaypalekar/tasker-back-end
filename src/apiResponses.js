29e49f2ab4add4847c6fe8cfebf098a9aef8af54
Akshay Palekar <apalekar@jahnelgroup.com>
2020-12-24 08:13:36 -0500

    adding headers

--- src/apiResponses.js
+++ src/apiResponses.js
@@ -4,7 +4,8 @@ const Responses = {
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': 'http://localhost:4200',
+                'Access-Control-Allow-Credentials': 'true'
             },
             statusCode: 200,
             body: JSON.stringify(data),
@@ -16,7 +17,8 @@ const Responses = {
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Methods': '*',
-                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': 'http://localhost:4200',
+                'Access-Control-Allow-Credentials': 'true'
             },
             statusCode: 400,
             body: JSON.stringify(data),
