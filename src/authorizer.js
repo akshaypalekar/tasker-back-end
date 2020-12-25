f6e114eb3a028fd2252b16f62d4f77b5ca7abe9e
Akshay Palekar <apalekar@jahnelgroup.com>
2020-12-25 10:36:49 -0500

    adding audiance

--- src/authorizer.js
+++ src/authorizer.js
@@ -4,9 +4,16 @@ const util = require('util');
 const LambdaUtils = require("./LambdaUtils");
 
 const jwtOptions = {
-    audience: "19ul0dh7n2d7mdfdf5m6tnds9o",
+    audience: "https://tddowj8otk.execute-api.us-east-1.amazonaws.com/dev",
     issuer: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_FBce5QEQc"
 };
+
+const client = jwksClient({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 10, // Default value
+    jwksUri: 'https://taskerdomain.auth.us-east-1.amazoncognito.com/.well-known/jwks.json'
+});
 
 exports.lambdaHandler = async (event) => {
     const token = LambdaUtils._getToken(event);
@@ -16,13 +23,6 @@ exports.lambdaHandler = async (event) => {
     if (!decoded || !decoded.header || !decoded.header.kid) {
         throw new Error('invalid token');
     }
-
-    const client = jwksClient({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 10, // Default value
-        jwksUri: 'https://taskerdomain.auth.us-east-1.amazoncognito.com/.well-known/jwks.json'
-    });
 
     const getSigningKey = util.promisify(client.getSigningKey);
 
