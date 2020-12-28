b7de572d20fb4171a2680bca8c92c8a913986403
Akshay Palekar <apalekar@jahnelgroup.com>
2020-12-28 15:10:57 -0500

    adding user context

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const LambdaUtils = require("./LambdaUtils");
 exports.lambdaHandler = async (event, context) => {
     console.info(`Request received: ${event.httpMethod}`);
     console.info("Request conext: " + JSON.stringify(context));
+    console.info("Request User: " + JSON.stringify(context.authorizer.user));
 
     switch (event.httpMethod) {
         case "POST":
